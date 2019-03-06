83c146e8facff28d0549e4251f716f8960e3449e
Rui Okada <okada.rui@gmail.com>
2019-03-05 16:58:18 -0800

    Add download for animation stickers

--- line-dl.py
+++ line-dl.py
@@ -6,6 +6,7 @@
 import argparse
 import os
 import re
+import sys
 
 from subprocess import call
 
@@ -24,6 +25,24 @@ REGEX_STICKER_SPAN = "".join((
 # style="background-image: url([URL];compress=true);"
 
 
+def download_file(url: str, save_path: str, name: str=""):
+    req = requests.get(url, stream=True)
+    total_length = req.headers.get('content-length')
+    if total_length is None:
+        raise Exception("Invalid response from server.")
+    else:
+        total_length = int(total_length)
+    with open(save_path, 'wb') as f:
+        total_data = 0
+        for data in req.iter_content(chunk_size=1024): 
+            if data:
+                total_data += len(data)
+                f.write(data)
+                done = int(100 * total_data / total_length)
+                sys.stdout.write(f"\rDownloading{' ' + name if name else name} {done}%")
+                sys.stdout.flush()
+
+
 def main():
     parser = argparse.ArgumentParser(description="Download Line Stickers in PNG format.")
     parser.add_argument('urls', metavar='URLs', type=str, nargs='+',
@@ -39,11 +58,14 @@ def main():
                   "Is Line down?"
                   )
             continue
-        else:
+        except:
             print("Something went wrong when opening the URL.",
                   "Is the URL valid?")
             continue
+
         try:
+            sticker_type = re.search(r'type: \"\w+\"', text).group(0)
+            sticker_type = sticker_type.replace("type: \"", "").replace("\"", "")
             sticker_count = len(re.findall(REGEX_STICKER_SPAN, text, flags=re.IGNORECASE))
             first_sticker = re.search(REGEX_STICKER_SPAN, text, flags=re.IGNORECASE)
 
@@ -53,33 +75,50 @@ def main():
             last_sticker_id = first_sticker_id + (sticker_count - 1)
 
             base_url = re.search(REGEX_STICKER_FILE_URL,
-                                 first_sticker.group(0))
+                                    first_sticker.group(0))
             base_url = base_url.group(0).replace(";compress=true", "")
 
-            dirname = re.search(r'<h3 class="\w+\">[\w\.,"\'\-; ]+<\/h3>',
-                                text).group(0).lower()
-            dirname = re.sub(r'<h3 class="\w+\">', "", dirname)
-            dirname = dirname.replace("</h3>", "")
-            dirname = re.sub(r'[^\w\&]', "-", dirname)
-        else:
+            stickerset_name = re.search(r'<h3 class="\w+\">[\w\.,"\'\-; ]+<\/h3>',
+                                text).group(0)
+            stickerset_name = re.sub(r'<h3 class="\w+\">', "", stickerset_name)
+            stickerset_name = stickerset_name.replace("</h3>", "")
+            dirname = re.sub(r'[^\w\&]', "-", stickerset_name).lower()
+        except:
             print("""
                 Requested page does not contain a sticker element.
                 Either Line has changed the webpage schema, or an invalid URL was used.
             """)
             continue
 
-        # try:
-        #     os.mkdir(dirname)
-        # except:
-        #     pass
-        # os.chdir(dirname)
+        try:
+            os.mkdir(dirname)
+        except Exception as inst:
+            pass
+        os.chdir(dirname)
 
-        # for i in range(first_sticker_id, last_sticker_id + 1):
-        #     sticker_url = base_url.replace(str(first_sticker_id), str(i))
-        #     print(sticker_url)
-        #     call(['curl', sticker_url, '-o', f'{str(i)}.png'])
+        # Download PNG Stickers
+        print("Downloading stickers from", stickerset_name)
+        for i in range(first_sticker_id, last_sticker_id + 1):
+            sticker_url = base_url.replace(str(first_sticker_id), str(i))
+            download_file(sticker_url, f'{str(i)}.png', f"Sticker {1 + i - first_sticker_id}/{sticker_count}")
+        print("\n")
+        os.chdir("..")
 
-        # os.chdir("..")
+        # Download PNG Animations
+        if sticker_type == "animation":
+            print("Downloading sticker animations from", stickerset_name)
+            try:
+                os.mkdir(f"{dirname}_animations")
+            except Exception as inst:
+                pass
+            os.chdir(f"{dirname}_animations")
+
+            animation_base_url = re.sub(r'android\/sticker.png', "IOS/sticker_animation@2x.png", base_url, flags=re.IGNORECASE)
+            for i in range(first_sticker_id, last_sticker_id + 1):
+                animation_url = animation_base_url.replace(str(first_sticker_id), str(i))
+                download_file(animation_url, f'{str(i)}.png', f"Animation {1 + i - first_sticker_id}/{sticker_count}")
+            print("")
+            os.chdir("..")
 
 
 if __name__ == "__main__":
